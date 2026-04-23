3a008c539a6a379294a98a80dfea06bfd9f165a7
Mihret Tadesse <mercytadesse40@gmail.com>
2026-04-23 11:50:51 +0300

    Implement user session check in add_comment.php
    Add user authentication check for commenting

--- php/add_comment.php
+++ php/add_comment.php
@@ -2,4 +2,13 @@
 // php/add_comment.php
 session_start();
 
+header('Content-Type: application/json');
+include 'db.php';
+
+$user_id = isset($_SESSION['user_id']) ? intval($_SESSION['user_id']) : 0;
+if ($user_id <= 0) {
+    echo json_encode(["status" => "error", "message" => "Please sign in to comment"]);
+    exit;
+}
+
 ?>
